3286592d75605f67e2a347f05e419b92edfec6c9
zerrorTwo <namle022004@gmail.com>
2025-06-22 14:07:13 +0700

    fix prisma and logic login gg

---
+++ prisma/migrations/20250622061507_add_gg_id/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `accounts` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `sessions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `verification_tokens` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `accounts` DROP FOREIGN KEY `accounts_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `googleId` VARCHAR(191) NULL;
+
+-- DropTable
+DROP TABLE `accounts`;
+
+-- DropTable
+DROP TABLE `sessions`;
+
+-- DropTable
+DROP TABLE `verification_tokens`;
